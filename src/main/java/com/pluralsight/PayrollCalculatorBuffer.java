e76472f9d7e009b74b1fd7762e693721479fe6ca
Randy23123 <rlopez@appdev.yearup.org>
2023-10-18 11:06:28 -0400

    done done

--- src/main/java/com/pluralsight/PayrollCalculatorBuffer.java
+++ src/main/java/com/pluralsight/PayrollCalculatorBuffer.java
@@ -1,19 +1,21 @@
 package com.pluralsight;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class PayrollCalculatorBuffer {
     public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Make a new file and include a .csv: ");
+        String secondFile = scanner.next();
 
         try {
             FileReader fileReader = new FileReader("src/main/resources/employees.csv");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            FileWriter fileEmployee = new FileWriter("src/main/resources/newEmployees.csv");
+            FileWriter fileEmployee = new FileWriter(secondFile);
             BufferedWriter bufWriter = new BufferedWriter(fileEmployee);
             String input;
 
-            bufWriter.write("111|Cameron Tay|3277.65\n" + "222|James Tee|2150.00");
-            bufWriter.close();
 
             while ((input = bufferedReader.readLine()) != null) {
                 String[] employee = input.split("\\|");
@@ -22,9 +24,11 @@ public class PayrollCalculatorBuffer {
                 double hoursWorked = Double.parseDouble(employee[2]);
                 double payRate = Double.parseDouble(employee[3]);
                 Employees down = new Employees(employeeId, name, hoursWorked, payRate);
-                System.out.printf("Employee Id: %d, Name: %s, GrossPay: $%.2f%n", down.getEmployeeId(), down.getName(), down.grossPay());
+                System.out.printf("Employee Id: %d, Name: %s, GrossPay: $%.2f%n ", down.getEmployeeId(), down.getName(), down.grossPay());
+                bufWriter.write("Employee Id: " + down.getEmployeeId() + "Name: " + down.getName() + "GrossPay: " + down.grossPay() + "\n");
             }
             bufferedReader.close();
+            bufWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
